f92d6dfdc8a527b6de34afbd7c546872326ff9c3
cryzicae-lgtm <cryzicae@gmail.com>
2026-04-16 07:04:50 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -32,7 +32,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "string edits",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 2, 
       },
     ],
   },
